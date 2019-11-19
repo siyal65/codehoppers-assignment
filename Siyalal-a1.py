66d5af1dfe13ce44ccde7f24f01c3ab9ddaefcd8
USER <au_guest@mcghi.mcg.edu>
2019-11-18 19:31:46 -0500

    added assignment1 code

---
+++ Siyalal-a1.py
@@ -0,0 +1,10 @@
+candy1 = "Twix"
+candy2 = "Gum"
+candy3 = "Reese's Pieces"
+timeofday = input("Enter the time of day (M,A,E): ")
+if (timeofday) == "M":
+    print("I will eat " + candy1 + " in the morning")
+elif (timeofday) == "A":
+    print("I will eat " + candy2 + " in the afternoon")
+else:
+    print ("I will eat " + candy3 + " in the evening")
